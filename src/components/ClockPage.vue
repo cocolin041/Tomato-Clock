8eb78946d0831a5fd25b4a2b6c0a7f56c290d7d5
Sih-Yi Lin <sihyiyl2@illinois.edu>
2019-07-11 16:18:57 +0800

    Init

---
+++ src/components/ClockPage.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="clock-page">
+    <div class="time">
+      <div class="missions">
+
+        <input placeholder="ADD A NEW MISSION…">
+        <div class="mission-header">
+          <div class="big-circle"></div>
+          <div class="name-and-circle">
+            <div class="mission-name">the First thing to do today</div>
+            <div class="small-circle"></div>
+          </div>
+        </div>
+
+        <div class="timer">25:00</div>
+
+      </div>
+
+      <div class="clock">
+        <div class="clock-border"></div>
+        <div class="clock-stroke"></div>
+        <div class="clock-inside"></div>
+      </div>
+    </div>
+
+    <div class="nav"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'ClockPage',
+  props: {
+
+  }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style lang="scss">
+    @import './clock-page.scss';
+</style>
